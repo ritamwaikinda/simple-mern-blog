4bbc8f4b80c6683de72341b80601b5b214995c1e
Rita Mwaikinda <srmwaikinda@gmail.com>
2020-11-07 02:12:41 -0500

    almost the end

---
+++ server/Article/article.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const ArticleSchema = new Schema({
+    title: String,
+    text: String,
+});
+
+
+module.exports = mongoose.model('Article', ArticleSchema);
---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const server = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb://localhost:27017/simple-mern-blog`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+const connection = mongoose.connection;
+connection.once('open', () => console.log("MongoDB connection established."));
+
+
+
+const PORT = process.env.port || 4000;
+server.listen(PORT, () => console.log(`Express server listening on port ${PORT}`));
+
+const articleRouter = require('./article/router');
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+
+server.use('/articles', articleRouter);
